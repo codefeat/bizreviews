8f61708f489739b5337b5ad229b03c13788e83b1
Lowell Fortune <lowellfortune@gmail.com>
2018-02-05 09:06:06 -0600

    commented hangin end tag

--- db/migrate/20180205125130_devise_create_admin_users.rb
+++ db/migrate/20180205125130_devise_create_admin_users.rb
@@ -34,7 +34,7 @@ class DeviseCreateAdminUsers < ActiveRecord::Migration
 
 
      # t.timestamps null: false
-    end
+    #end
 
     #add_index :admin_users, :email,                unique: true
     #add_index :admin_users, :reset_password_token, unique: true
